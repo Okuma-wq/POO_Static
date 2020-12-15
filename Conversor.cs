e92b3b405fe9a8b42ecbbcde9a5eef8d451bfa49
pedro Okuma <pedrookuma3@gmail.com>
2020-12-15 12:52:17 -0300

    POO Static concluido

---
+++ Conversor.cs
@@ -0,0 +1,25 @@
+namespace POO_Static
+{
+    public static class Conversor
+    {
+        private static float cotacaoDolar = 5.10f;
+        private static float cotacaoEuro = 6.21f;
+        public static float valorUsuario;
+
+        public static float ConverterDolarParaReal(){
+            return valorUsuario * cotacaoDolar;
+        }
+
+        public static float ConverterRealParaDolar(){
+            return valorUsuario / cotacaoDolar;
+        }
+
+        public static float ConverterEuroParaReal(){
+            return valorUsuario * cotacaoEuro;
+        }
+
+        public static float ConverterRealParaEuro(){
+            return valorUsuario / cotacaoEuro;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace POO_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string escolha = "";
+            do{
+                Console.WriteLine("Qual conversão deseja realizar?");
+                Console.WriteLine("[1] - Real para Dolar");
+                Console.WriteLine("[2] - Dolar para Real");
+                Console.WriteLine("[3] - Real para Euro");
+                Console.WriteLine("[4] - Euro para Real");
+                Console.WriteLine("[0] - Sair");
+                escolha = Console.ReadLine();
+                if(escolha == "1" || escolha == "2" || escolha == "3" || escolha == "4"){
+                    Console.Write("Digite um valor: ");
+                    Conversor.valorUsuario = float.Parse(Console.ReadLine());
+                }
+                
+                switch (escolha)
+                {
+                    case "1":
+                        Console.WriteLine($"O valor convertido é de R${Conversor.ConverterRealParaDolar()}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"O valor convertido é de ${Conversor.ConverterDolarParaReal()}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"O valor convertido é de €{Conversor.ConverterRealParaEuro()}");
+                        break;
+                    case "4":
+                        Console.WriteLine($"O valor convertido é de R${Conversor.ConverterEuroParaReal()}");
+                        break;
+                    case "0":
+                        Console.WriteLine("Encerrando programa");
+                        break;
+                    default:
+                        Console.WriteLine("Escolha inválida");
+                        break;
+                }
+                
+
+
+            }while(escolha != "0");
+        }
+    }
+}
